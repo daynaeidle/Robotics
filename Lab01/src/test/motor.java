2d7473e182748dafce75f42c45f73e964402535a
daynaeidle <dayna.eidle1@marist.edu>
2020-02-06 23:58:16 -0500

    Uploading motor test file.

---
+++ Lab01/src/test/motor.java
@@ -0,0 +1,42 @@
+package test;
+
+import lejos.hardware.Keys;
+import lejos.hardware.BrickFinder;
+import lejos.hardware.ev3.EV3;
+import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+
+public class motor {
+
+	public static void main(String[] args) {
+		
+		EV3LargeRegulatedMotor LEFT_MOTOR = new EV3LargeRegulatedMotor(MotorPort.A);
+		EV3LargeRegulatedMotor RIGHT_MOTOR = new EV3LargeRegulatedMotor(MotorPort.B);
+		
+		EV3 ev3brick = (EV3) BrickFinder.getLocal();
+		
+		Keys buttons = ev3brick.getKeys();
+		
+		buttons.waitForAnyPress();
+		
+		LEFT_MOTOR.forward();
+		RIGHT_MOTOR.forward();
+		LCD.drawString("Forward", 0, 0);
+		
+		buttons.waitForAnyPress();
+		
+		LEFT_MOTOR.backward();
+		RIGHT_MOTOR.backward();
+		LCD.drawString("Backward", 0, 1);
+		
+		buttons.waitForAnyPress();
+		
+		LEFT_MOTOR.stop();
+		RIGHT_MOTOR.stop();
+		LCD.drawString("Stop", 0, 2);
+		
+
+	}
+
+}
