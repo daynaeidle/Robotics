2c2c59b35f08f1ccc0b468dcbe3479731ebb28d5
Ryan Pepe <ryan.pepe1@marist.edu>
2020-02-29 21:37:36 -0500

    Commented PIDController classes and updated Javadocs

--- Lab02/src/PIDController.java
+++ Lab02/src/PIDController.java
@@ -2,12 +2,11 @@ import lejos.hardware.sensor.NXTUltrasonicSensor;
 import lejos.robotics.SampleProvider;
 
 public class PIDController {
-	private float kp, ki, kd, currError, prevError, integral, currDistance;
+	private float kp, ki, kd, currError, prevError, currDistance, proportion, integral, derivative;
 	private int sampleSize;
 	private float[] sample;
 	private SampleProvider sp;
 	private NXTUltrasonicSensor sensor;
-	private final int DEFAULT_SPEED = 360;
 	private final float GOAL_DISTANCE = 25;
 	
 	PIDController(){
@@ -15,8 +14,25 @@ public class PIDController {
 		this.ki = 0;
 		this.kd = 0;
 		this.integral = 0;
+		this.sampleSize = 0;
+		this.sample = null;
+		this.sensor = null;
+		this.sp = null;
+		this.currError = 0;
 	}
 	
+	/**
+	 * PIDController
+	 * 
+	 * This constructor takes a sensor, and the constants being use on the PID controller. The
+	 * Constructor will contain all of the logic necessary for computing samples, distance, and
+	 * error.
+	 * 
+	 * @param sensor
+	 * @param kp
+	 * @param ki
+	 * @param kd
+	 */
 	PIDController(NXTUltrasonicSensor sensor, float kp, float ki, float kd){
 		this.sensor = sensor;
 		this.kp = kp;
@@ -24,61 +40,72 @@ public class PIDController {
 		this.kd = kd;
 		this.integral = 0;
 		this.currDistance = 0;
+		this.currError = 0;
 		this.sp = this.sensor.getDistanceMode();
 		this.sampleSize = sp.sampleSize();
 		this.sample = new float[sampleSize];
 	}
 	
-	private float fetchSample() {
+	
+	/**
+	 * fetchSample
+	 * 
+	 * This method gets the current sample from the sensor, and it converts
+	 * it to a distance in cm. This method is private because using it in
+	 * a different class could result in unsynchronized advances of the sample.
+	 */
+	private void fetchSample() {
 		this.sp.fetchSample(this.sample, 0);
-		return sample[0];
+		this.currDistance = sample[0] * 100;
 	}
 	
 	/**
 	 * getError
 	 * 
 	 * Gets the magnitude and direction of the current state of error within the system.
+	 * This method is also used to advance the state of the current sample
 	 * @return The amount of error in the system's current state
 	 */
-	public float getError() {
+	private float getError() {
+		fetchSample();
 		this.prevError = this.currError;
-		this.currDistance = fetchSample();
-		return GOAL_DISTANCE - this.currDistance;
+		this.currError = GOAL_DISTANCE - this.currDistance;
+		// We could put a cap on the current error here
+		return this.currError;
 	}
 	
+	/**
+	 * computePID
+	 * 
+	 * Calculates the amount of speed to be applied to the current system using 
+	 * proportions, integrals, and derivatives
+	 * 
+	 * @return Amount of speed to added to the motor
+	 */
 	public float computePID() {
 		getError();
-		float proportion = this.currError;
-		this.integral += this.currError;
-		float derivative = currError - prevError;
-		return (proportion * kp) + (this.integral * ki) + (derivative * kd);
+		// P will be the amount of current error
+		this.proportion = this.currError;
+		
+		// I will be the sum of all current error in the system. Limit is set at 50 (needs to be tuned)
+		if (this.integral < 50 && this.integral > -50) {
+			this.integral += this.currError;
+		} else if (this.integral > 50) {
+			this.integral = 50;
+		} else if (this.integral < -50) {
+			this.integral = -50;
+		}
+		// D will be the amount of change between the error last detected and the current error
+		this.derivative = prevError - currError;
+		return (this.proportion * kp) + (this.integral * ki) + (this.derivative * kd);
 	}
 	
-	
-
-	public float getKp() {
-		return kp;
-	}
-
-	public void setKp(float kp) {
-		this.kp = kp;
-	}
-
-	public float getKi() {
-		return ki;
-	}
-
-	public void setKi(float ki) {
-		this.ki = ki;
-	}
-
-	public float getKd() {
-		return kd;
-	}
-
-	public void setKd(float kd) {
-		this.kd = kd;
+	public float getCurrError() {
+		return this.currError;
 	}
 	
+	public float getCurrDistance() {
+		return this.currDistance;
+	}
 	
 }
--- Lab02/src/WallFollower.java
+++ Lab02/src/WallFollower.java
@@ -13,14 +13,10 @@ public class WallFollower {
 	public static void main(String[] args) {
 
 		final int DEFAULT_SPEED = 360;
-		final float GOAL_DISTANCE = 25;
-		final float Kp = 15;
+		final float Kp = 100;
 		final float Ki = 1;
-		final float Kd = (float) 0.5;
+		final float Kd = (float) 1;
 		final float THRESHOLD = 50;
-
-		float distance;
-		float integral = 0;
 
 		EV3LargeRegulatedMotor LEFT_MOTOR = new EV3LargeRegulatedMotor(MotorPort.A);
 		EV3LargeRegulatedMotor RIGHT_MOTOR = new EV3LargeRegulatedMotor(MotorPort.D);
@@ -36,77 +32,28 @@ public class WallFollower {
 		int sampleSize1 = sp1.sampleSize();
 		float[] sample1 = new float[sampleSize1];
 
-		float pError = 0;
-		float prevError = 0;
-
+		PIDController controller = new PIDController(sideSensor, Kp, Ki, Kd);
+		buttons.waitForAnyPress();
 		LEFT_MOTOR.backward();
 		RIGHT_MOTOR.backward();
-
+		
 		while (true) {
 
-			sp1.fetchSample(sample1, 0);
-			LCD.drawString(String.valueOf(sample1[0] * 100), 1, 0);
+			LCD.drawString("Error: " + String.valueOf(controller.getCurrError()), 1, 2);
 
-			if (sample1[0] * 100 >= THRESHOLD) {
-				distance = THRESHOLD;
-			} else {
-				distance = sample1[0] * 100;
-			}
-
-			pError = GOAL_DISTANCE - distance;
-
-			LCD.drawString("pError: " + String.valueOf(pError), 1, 2);
-
-			integral = updateIntegral(integral, pError);
-
-			if (pError < 0) {
-
-				LEFT_MOTOR.setSpeed(DEFAULT_SPEED + (calculateSpeed(Kp, Ki, Kd, pError, prevError, integral)) * -1);
+			if (controller.getCurrError() < 0) {
+				LEFT_MOTOR.setSpeed(DEFAULT_SPEED + (controller.computePID()) * -1);
 				RIGHT_MOTOR.setSpeed(DEFAULT_SPEED);
+			} else if (controller.getCurrError() > 0) {
 
-			} else if (pError > 0) {
-
-				RIGHT_MOTOR.setSpeed((DEFAULT_SPEED + (calculateSpeed(Kp, Ki, Kd, pError, prevError, integral))));
+				RIGHT_MOTOR.setSpeed((DEFAULT_SPEED + (controller.computePID())));
 				LEFT_MOTOR.setSpeed(DEFAULT_SPEED);
 
-			} else if (pError == 0) {
+			} else if (controller.getCurrError() == 0) {
 				RIGHT_MOTOR.setSpeed(DEFAULT_SPEED);
 				LEFT_MOTOR.setSpeed(DEFAULT_SPEED);
+				controller.computePID();
 			}
-
-			// if error is negative, increase speed of the right motor
-			// if error is positive, increase the speed of the left motor
-			prevError = pError;
-
 		}
-
 	}
-
-	static float calculateSpeed(float kp, float ki, float kd, float error, float prevError, float integral) {
-
-		float p = kp * error;
-
-		float i = ki * integral;
-
-		float d = kd * updateDerivative(error, prevError);
-
-		return p + i + d;
-
-	}
-
-	static float updateIntegral(float integral, float error) {
-		if (error > 10) {
-			return integral + 10;
-		} else if (error < -10) {
-			return integral - 10;
-		} else {
-			return integral + error;
-		}
-
-	}
-
-	static float updateDerivative(float error, float prevError) {
-		return prevError - error;
-	}
-
 }
